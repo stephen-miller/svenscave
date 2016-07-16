ae69e2e672be884f491b9f481842a9ebb6b69e60
Stephen Miller <stephen.miller7@gmail.com>
2016-07-16 12:37:36 -0400

    cant get view resolver to recognise home, guess is that it has to do with the explicit request mapping

--- src/main/java/org/webservice/WebSecurityConfig.java
+++ src/main/java/org/webservice/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package org.webservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 /**
  * Created by Sven on 7/10/2016.
--- src/main/java/org/webservice/controller/HelloController.java
+++ src/main/java/org/webservice/controller/HelloController.java
@@ -13,5 +13,7 @@ public class HelloController extends WebMvcConfigurerAdapter {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/addressBook").setViewName("addressBook");
+        registry.addViewController("/familyTree").setViewName("familyTree");
     }
 }
