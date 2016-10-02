6aaab3d29cd02df2f57be7aa7bc1aec76875c970
Stephen Miller <stephen.miller7@gmail.com>
2016-10-02 13:43:30 -0400

    ripped out all the crap and got it working

--- src/main/java/org/webservice/controller/HelloController.java
+++ src/main/java/org/webservice/controller/HelloController.java
@@ -1,10 +1,11 @@
 package org.webservice.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@RestController
+@Controller
 public class HelloController extends WebMvcConfigurerAdapter {
 
     @Override
@@ -12,8 +13,11 @@ public class HelloController extends WebMvcConfigurerAdapter {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+        //registry.addViewController("/login").setViewName("login");
         registry.addViewController("/addressBook").setViewName("addressBook");
         registry.addViewController("/familyTree").setViewName("familyTree");
     }
+
+
+
 }
