38245ba765d21ec9050bed5cfbf421a6ab86c292
Stephen Miller <stephen.miller7@gmail.com>
2016-10-02 14:21:59 -0400

    changed .gitignore and added endpoint.java

---
+++ addressbook/src/main/java/com/addressbook/controller/Endpoint.java
@@ -0,0 +1,12 @@
+package com.addressbook.controller;
+
+/**
+ * This is the endpoint of the AddressBook microservice
+ *
+ * Created by Sven on 10/2/2016.
+ */
+@RestController
+public class Endpoint {
+
+
+}
